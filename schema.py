c86d786ffb1dcde030c52f83a33eaf9824d1c6e1
lntvan1606 <vanlnt@ihouzz.com>
2022-06-11 08:25:17 +0700

    feat(database): user service

--- schema.py
+++ schema.py
@@ -1,7 +1,38 @@
+from ctypes import sizeof
+import email
 import mongoengine as me
+from requests import session
 
 
 class User(me.Document):
-    username = me.StringField()
+    email = me.StringField()
+    name = me.StringField()
+    dateOfBirth = me.DateTimeField()
+    phone = me.StringField()
+    address = me.StringField()
     password = me.StringField()
+    publicKey = me.StringField()
+    privateKey = me.StringField()
     meta = {"collection": "users"}
+
+class FileCrypto(me.Document):
+    name = me.StringField()
+    path = me.StringField()
+    content = me.BinaryField()
+    size = me.IntField()
+    sessionKey = me.StringField()
+    meta = {"collection": "files"}
+
+class SignedFile(me.Document):
+    name = me.StringField()
+    path = me.StringField()
+    content = me.BinaryField()
+    size = me.IntField()
+    meta = {"collection": "filesignatures"}
+
+class SignFile(me.Document):
+    name = me.StringField()
+    path = me.StringField()
+    content = me.BinaryField()
+    size = me.IntField()
+    meta = {"collection": "signfiles"}
--- src/api/auth/service.py
+++ src/api/auth/service.py
@@ -3,3 +3,30 @@ from schema import User
 
 def getOneUser(username):
     return User.objects(username=username).first()
+
+def getAllUsers():
+    return User.objects()
+
+def addUser(email, name, dateOfBirth, phone, address, password, publicKey, privateKey):
+    user = User(email=email, name=name, dateOfBirth=dateOfBirth, phone=phone, address=address, password=password, publicKey=publicKey, privateKey=privateKey)
+    user.save()
+    return user
+
+def updateUser(username, email, name, dateOfBirth, phone, address, password, publicKey, privateKey):
+    user = User.objects(username=username).first()
+    user.email = email
+    user.name = name
+    user.dateOfBirth = dateOfBirth
+    user.phone = phone
+    user.address = address
+    user.password = password
+    user.publicKey = publicKey
+    user.privateKey = privateKey
+    user.save()
+    return user
+
+def updatePassword(username, password):
+    user = User.objects(username=username).first()
+    user.password = password
+    user.save()
+    return user
